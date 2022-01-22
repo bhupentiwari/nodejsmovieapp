6840af715cf3c5078d777b05ccaf4bdf1ccf9702
Tiwari <bhupentiwari@live.com>
2022-01-22 18:59:06 +0530

    Request object and Middleware

--- app.js
+++ app.js
@@ -6,19 +6,25 @@ const app = express();
 //json
 //redirect
 
-// app.get('/',(req,res,next)=>{
-// res.status(200).send('Weclome to the home page')
+app.use((req,res,next)=>{
+console.log(req.ip);
+next();
+});
+
+app.get('/',(req,res,next)=>{
+res.status(200).send('Weclome to the home page')
 // res.json({
 //     message : "Welcome to the home page"
 // });
-// });
+});
 
-app.get('/',(req,res,next)=>{
-    res.redirect('/user');
-    });
+// app.get('/',(req,res,next)=>{
+//     res.redirect('/user');
+//     });
 
     
-app.get('/user',(req,res,next)=>{
-    res.status(200).send('Weclome to the user home page')
+app.get('/user/:id/:postId',(req,res,next)=>{
+    console.log(req.params);
+    res.status(200).send('Weclome to the user homepage')
 })
 module.exports = app;
