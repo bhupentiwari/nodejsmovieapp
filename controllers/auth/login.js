73b0913d0d22a7d7c0d3d75b2b751dac20c0c85f
Tiwari <bhupentiwari@live.com>
2022-01-22 20:14:04 +0530

    Logging tools

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,3 +1,7 @@
+const logger = require('../../configuration/logger');
+
 module.exports.getLogin = (req,res,next)=>{
+    logger.info('Hello I am from logger');
     res.send('Welcome to login page getlogin method');
+    
 };
