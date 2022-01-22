50a8ec3b898185b73fd1341b5953025fdfb96141
Tiwari <bhupentiwari@live.com>
2022-01-22 08:27:32 +0530

    Route handling

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 const express = require('express');
 const app = express();
+
+//send
+//status
+//json
+//redirect
+
+// app.get('/',(req,res,next)=>{
+// res.status(200).send('Weclome to the home page')
+// res.json({
+//     message : "Welcome to the home page"
+// });
+// });
+
+app.get('/',(req,res,next)=>{
+    res.redirect('/user');
+    });
+
+    
+app.get('/user',(req,res,next)=>{
+    res.status(200).send('Weclome to the user home page')
+})
 module.exports = app;
